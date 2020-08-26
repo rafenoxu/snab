bf19cd007a842c2b02f744f352e11591ed8c0c7a
rafenoxu <skotnicki.rafal@gmail.com>
2020-08-26 14:49:53 +0200

    Added rest of animations; From now on you can't scroll when mobile menu is open; case study positioning

--- js/navbar.js
+++ js/navbar.js
@@ -4,11 +4,13 @@ const navbarToggle = navbar.querySelector(".menu-toggle");
 function openMobileNavbar() {
     navbar.classList.add("opened");
     navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+    document.body.style.position = 'fixed';
 }
 
 function closeMobileNavbar() {
     navbar.classList.remove("opened");
     navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+    document.body.style.position = '';
 }
 
 navbarToggle.addEventListener("click", () => {
